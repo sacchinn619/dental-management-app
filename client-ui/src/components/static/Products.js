70d646e576397a7aa693f946583dcaa1a09c51d9
SACHIN RAJ <sacchinn619@gmail.com>
2020-08-27 21:45:49 +0530

    changed theme

--- client-ui/src/components/static/Products.js
+++ client-ui/src/components/static/Products.js
@@ -42,7 +42,7 @@ return(
       <Card.Text>
         This is a wider card with supporting text below as a natural lead-in to
         additional content. This card has even longer content than the first to
-        show that equal height action.
+        show that equal height action.hi there
       </Card.Text>
     </Card.Body>
     <Card.Footer>
