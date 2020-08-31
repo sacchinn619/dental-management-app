fcf0fd5240817c33eb54a4c8419aaf81a557aaf7
sachin noel raj <sacchinn619@gmail.com>
2020-08-31 19:51:00 +0530

    Update configureStore.js

--- client-ui/src/store/configureStore.js
+++ client-ui/src/store/configureStore.js
@@ -2,11 +2,13 @@ import {createStore,combineReducers,applyMiddleware} from 'redux'
 import thunk from 'redux-thunk'
 import userReducer from '../reducers/userReducer'
 import appointmentReducer from '../reducers/appointmentReducer'
+import reviewsReducer from '../reducers/reviewsReducer'
 
 const configureStore=()=>{
 const store=createStore(combineReducers({
    user:userReducer,
-   appointment:appointmentReducer
+   appointment:appointmentReducer,
+   reviews:reviewsReducer
 }),applyMiddleware(thunk))
 return store
 }
