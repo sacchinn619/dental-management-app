c7e0b906d5fd63e31bd4ccdb39f1934f3dde6606
sachin noel raj <sacchinn619@gmail.com>
2020-08-31 19:53:37 +0530

    Update routes.js

--- config/routes.js
+++ config/routes.js
@@ -4,6 +4,7 @@ const usersController = require('../app/controllers/usersController')
 const appointmentsController= require('../app/controllers/appointmentsController')
 const productsController= require ('../app/controllers/productsController')
 const authenticateUser = require('../app/middlewares/authentication')
+const reviewsController= require('../app/controllers/reviewsController')
 
 router.post('/api/users/register', usersController.register)
 router.post('/api/users/login', usersController.login)
@@ -13,6 +14,8 @@ router.put('/api/users/appointment/:id',appointmentsController.update)
 router.get('/api/users/appointments',authenticateUser,appointmentsController.get)
 router.post('/api/products/info', productsController.info)
 router.get('/api/products/:id', productsController.product)
+router.post('/api/reviews',reviewsController.post)
+router.get('/api/reviews',reviewsController.get)
 
 
 module.exports = router
